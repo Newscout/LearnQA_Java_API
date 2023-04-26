c3559b4732fd91e3867f34b590b69206fb1c689a
newscout <d.ognev@unitedtraders.com>
2023-04-26 19:39:52 +0200

    add new test for Ex5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
@@ -12,4 +13,13 @@ public class HelloWorldTest {
                 .andReturn();
         response.prettyPrint();
     }
+    @Test
+    public void testEx5ParsingJson() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        String answer = response.get("messages[1].message");
+        System.out.println(answer);
+    }
+
 }
