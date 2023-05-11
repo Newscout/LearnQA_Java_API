3a398f949452a6352661f8420b4d37e238b46dfd
newscout <d.ognev@unitedtraders.com>
2023-05-11 11:49:41 +0200

    Add new test for Ex11

--- src/test/java/Lesson3Tests.java
+++ src/test/java/Lesson3Tests.java
@@ -1,4 +1,8 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,5 +11,15 @@ public class Lesson3Tests {
     public void testEx10ShortPhrase() {
         String phrase = "Hello, World!";
         assertTrue(phrase.length() > 15, "This phrase is too short!!!");
+    }
+    @Test
+    public void testEx11Cookies() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        Map<String, String> cookies = response.getCookies();
+        assertFalse(cookies.isEmpty(), "There is no cookies.");
+        assertTrue(cookies.containsKey("HomeWork"),"There is no cookie with this key.");
+        assertTrue(cookies.containsValue("hw_value"),"There is no cookie with this value.");
     }
 }
